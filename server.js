8c8c27e09d38a412127acc15b957d0f4950e33fb
Rahul Singh Bisht <bishtrahulsingh34@gmail.com>
2023-07-17 11:38:18 +0530

    cors

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const path = require('path');
 const cors = require('cors');
 // Cors 
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
 
 app.use(cors(corsOptions))
 app.use(express.static('public'));
